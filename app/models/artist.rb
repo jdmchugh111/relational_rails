16c4a7eedfdff951b406ae065d9e2f34ae92039f
James McHugh <jdmchugh111@gmail.com>
2024-05-14 01:09:23 -0400

    Refactored

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,6 @@
 class Artist < ApplicationRecord
     belongs_to :festival
 
-    # def self.sort_by_name
-    #     Artist.order(:name)
-    # end
     def self.filter(number)
         Artist.where("performers > #{number}")
     end
